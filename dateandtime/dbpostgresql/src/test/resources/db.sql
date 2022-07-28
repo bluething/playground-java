2c4192fa5acc537614bb591be7b650947eabaa09
Habib Ali Machpud <habib.machpud@gmail.com>
2022-07-28 14:08:16 +0700

    Update db.sql
    Add more data

--- dateandtime/dbpostgresql/src/test/resources/db.sql
+++ dateandtime/dbpostgresql/src/test/resources/db.sql
@@ -5,3 +5,10 @@ CREATE TABLE person (
 	PRIMARY KEY (id)
 );
 INSERT INTO person (id, name, date_of_birth) VALUES (1, 'fulan', TO_TIMESTAMP('2000-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (2, 'fulan' 2, TO_TIMESTAMP('2000-01-10 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (3, 'fulan 3', TO_TIMESTAMP('2000-01-20 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (4, 'fulan 4', TO_TIMESTAMP('2000-01-21 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (5, 'fulan 5', TO_TIMESTAMP('2000-01-22 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (6, 'fulan 6', TO_TIMESTAMP('2000-01-22 01:00:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (7, 'fulan 7', TO_TIMESTAMP('2000-01-22 01:10:00', 'YYYY-MM-DD HH24:MI:SS'));
+INSERT INTO person (id, name, date_of_birth) VALUES (8, 'fulan 8', TO_TIMESTAMP('2000-01-22 23:00:00', 'YYYY-MM-DD HH24:MI:SS'));
