511442cddeae1b2e8eb5f2f917312141b0975661
Habib Ali Machpud <habib.machpud@gmail.com>
2022-08-11 22:19:46 +0700

    Init

---
+++ dateandtime/dateandtimeinspringmvc/src/test/java/io/github/bluething/playground/java/dateandtime/dateandtimeinspringmvc/DateandtimeinspringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.java.dateandtime.dateandtimeinspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateandtimeinspringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
