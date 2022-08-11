950d174ba57064b2f738304af15046747b9ef787
Habib Ali Machpud <habib.machpud@gmail.com>
2022-08-11 22:03:31 +0700

    Create ClockTest.java

---
+++ dateandtime/intro/src/test/java/io/github/bluething/playground/java/dateandtime/date/ClockTest.java
@@ -0,0 +1,60 @@
+package io.github.bluething.playground.java.dateandtime.date;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.*;
+
+public class ClockTest {
+    @Test
+    public void create() {
+        Clock clockUTC = Clock.systemUTC();
+        System.out.println(clockUTC);
+
+        Clock clockDefault = Clock.systemDefaultZone();
+        System.out.println(clockDefault);
+
+        Clock clockCustomZone = Clock.system(ZoneId.of("Asia/Jakarta"));
+        System.out.println(clockCustomZone);
+    }
+
+    @Test
+    public void instant() throws InterruptedException {
+        Clock clock = Clock.system(ZoneId.of("Asia/Jakarta"));
+
+        Instant instant1  = clock.instant();
+        System.out.println(instant1);
+
+        Thread.sleep(1000);
+        Instant instant2 = clock.instant();
+        System.out.println(instant2);
+    }
+
+    @Test
+    public void fromClock() {
+        Clock clock = Clock.system(ZoneId.of("Asia/Jakarta"));
+
+        Year year = Year.now(clock);
+        System.out.println(year);
+
+        YearMonth yearMonth = YearMonth.now(clock);
+        System.out.println(yearMonth);
+
+        MonthDay monthDay = MonthDay.now(clock);
+        System.out.println(monthDay);
+
+        LocalTime localTime = LocalTime.now(clock);
+        System.out.println(localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.now(clock);
+        System.out.println(localDateTime);
+
+        OffsetTime offsetTime = OffsetTime.now(clock);
+        System.out.println(offsetTime);
+
+        OffsetDateTime offsetDateTime = OffsetDateTime.now(clock);
+        System.out.println(offsetDateTime);
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(clock);
+        System.out.println(zonedDateTime);
+    }
+}
