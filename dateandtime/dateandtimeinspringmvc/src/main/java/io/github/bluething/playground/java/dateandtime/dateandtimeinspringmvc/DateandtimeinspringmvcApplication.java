b8d33eefc67b64e9d1223e41a79f2c94eaed4e4b
Habib Ali Machpud <habib.machpud@gmail.com>
2022-08-11 22:57:06 +0700

    Init module

---
+++ dateandtime/dateandtimeinspringmvc/src/main/java/io/github/bluething/playground/java/dateandtime/dateandtimeinspringmvc/DateandtimeinspringmvcApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.java.dateandtime.dateandtimeinspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateandtimeinspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateandtimeinspringmvcApplication.class, args);
+	}
+
+}
