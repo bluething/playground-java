0debcb7b1bf3b5762e0e78fca1417148144d1864
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-29 22:59:46 +0700

    Sample of lamba implementing functional interface

--- lambda/pluralsight_lambdaexpressionjava8/src/main/java/io/github/bluething/playground/java/lambda/pluralsight_lambdaexpressionjava8/Main.java
+++ lambda/pluralsight_lambdaexpressionjava8/src/main/java/io/github/bluething/playground/java/lambda/pluralsight_lambdaexpressionjava8/Main.java
@@ -1,7 +1,16 @@
 package io.github.bluething.playground.java.lambda.pluralsight_lambdaexpressionjava8;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Supplier<String> supplier = () -> "Hii";
+        System.out.println("supplier: " + supplier.get());
+
+        Consumer<String> consumer = s -> System.out.println("consume: " + s);
+        consumer.accept(supplier.get());
     }
 }
