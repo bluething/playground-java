50d76f3e017cc83d41bea5dacaddec0515913bd8
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-29 23:22:31 +0700

    Add some description

--- lambda/pluralsight_lambdaexpressionjava8/src/main/java/io/github/bluething/playground/java/lambda/pluralsight_lambdaexpressionjava8/Main.java
+++ lambda/pluralsight_lambdaexpressionjava8/src/main/java/io/github/bluething/playground/java/lambda/pluralsight_lambdaexpressionjava8/Main.java
@@ -26,5 +26,17 @@ public class Main {
 
         DoubleToIntFunction doubleToIntFunction = value -> (int) Math.floor(value);
         System.out.println("pii: " + doubleToIntFunction.applyAsInt(Math.PI));
+
+        /**
+         * a lambda is an instance of a functional interface
+         *
+         * lambda expressions have been very aggressively optimized in Java
+         *  How fast lambda expressions are?
+         *      Lambda expressions are compiled using specific bytecode instructions called invokedynamic introduced in Java 7
+         *  What you want to avoid when writing lambda expressions is a mechanism introduced in Java 5 called autoboxing
+         *
+         *  when you're dealing with numbers, whether they are ints, longs, and doubles,
+         *      you need to remember that you have specialized lambda expressions that are going to be much faster than the regular ones
+        * */
     }
 }
